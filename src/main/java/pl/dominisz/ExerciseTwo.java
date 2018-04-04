939a25d6a40bc62728b074a19fc59c9fd70ef15d
dominisz <dominisz@gmail.com>
2018-04-04 17:48:44 +0200

    Implementation of classes for ExerciseTwo

--- src/main/java/pl/dominisz/ExerciseTwo.java
+++ src/main/java/pl/dominisz/ExerciseTwo.java
@@ -4,11 +4,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Exercise from
  * https://docs.oracle.com/javase/tutorial/collections/streams/QandE/questions.html
- *
+ * <p>
  * Convert the following code into a new implementation that uses lambda expressions and aggregate operations
  * instead of nested for loops.
  * Hint: Make a pipeline that invokes the filter, sorted, and collect operations, in that order.
@@ -16,25 +17,19 @@ import java.util.List;
 public class ExerciseTwo {
 
     public static void main(String[] args) {
-        List<Album> albums = new ArrayList<>();
-        List<Album> favs = new ArrayList<>();
+        List<Album> albums = createAlbums();
+        List<Album> favs = albums.stream()
+                .filter(album -> album.hasFavorite())
+                .sorted((album1, album2)
+                        -> album1.getName().compareTo(album2.getName()))
+                .collect(Collectors.toList());
+        showAlbums(favs);
+    }
 
-        for (Album a : albums) {
-            boolean hasFavorite = false;
-            for (Track t : a.getTracks()) {
-                if (t.getRating() >= 4) {
-                    hasFavorite = true;
-                    break;
-                }
-            }
-            if (hasFavorite)
-                favs.add(a);
-        }
+    private static void showAlbums(List<Album> albums) {
+    }
 
-        Collections.sort(favs, new Comparator<Album>() {
-            public int compare(Album a1, Album a2) {
-                return a1.getName().compareTo(a2.getName());
-            }
-        });
+    private static List<Album> createAlbums() {
+        return new ArrayList<>();
     }
 }
